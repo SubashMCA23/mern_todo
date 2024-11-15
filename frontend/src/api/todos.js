ba1cba54a257df40c7431ffb889794676f8bee94
SubashMCA23 <139734871+SubashMCA23@users.noreply.github.com>
2024-11-15 10:06:01 +0530

    Update todos.js

--- frontend/src/api/todos.js
+++ frontend/src/api/todos.js
@@ -1,7 +1,7 @@
 // client/src/api/todos.js
 import axios from 'axios';
 
-const API_URL = ""https://mern-todo-backend-hr3u.onrender.com/api/todos"";
+const API_URL = "https://mern-todo-backend-hr3u.onrender.com/api/todos";
 
 export const getTodos = () => axios.get(API_URL);
 export const addTodo = (newTodo) => axios.post(API_URL, newTodo);
