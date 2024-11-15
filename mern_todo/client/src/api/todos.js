534026b567bf5b6a4961ad77f9204c9a8407a647
SubashMCA23 <139734871+SubashMCA23@users.noreply.github.com>
2024-11-15 09:49:03 +0530

    Delete mern_todo directory

--- mern_todo/client/src/api/todos.js
+++
@@ -1,9 +0,0 @@
-// client/src/api/todos.js
-import axios from 'axios';
-
-const API_URL = "http://localhost:5000/api/todos";
-
-export const getTodos = () => axios.get(API_URL);
-export const addTodo = (newTodo) => axios.post(API_URL, newTodo);
-export const updateTodo = (id, updatedTodo) => axios.patch(`${API_URL}/${id}`, updatedTodo);
-export const deleteTodo = (id) => axios.delete(`${API_URL}/${id}`);
